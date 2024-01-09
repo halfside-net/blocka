e3338baae96a05340753de5f5b007db77c0cbb43
halfside-net <105025415+halfside-net@users.noreply.github.com>
2024-01-08 22:04:48 -0500

    import manifest icons as metadata to get sizes

--- pages/webmanifest.json.page.client.ts
+++ pages/webmanifest.json.page.client.ts
@@ -1,17 +1,20 @@
 import { description, sitePath, themeColor, title, titleShort } from '../siteconfig.json';
-import icons from '~/assets/images/icon.png?w=72;96;120;128;144;152;180;192;384;512';
-
-const iconSizes = [72, 96, 120, 128, 144, 152, 180, 192, 384, 512];
+import icons from '~/assets/images/icon.png?as=metadata&w=72;96;120;128;144;152;180;192;384;512';
 
 export function render() {
   return JSON.stringify({
     background_color: themeColor,
     description,
     display: 'standalone',
-    icons: iconSizes.map((size, i) => ({
-      sizes: `${size}x${size}`,
-      src: icons[i],
-      type: 'image/png'
+    icons: icons.map((icon: {
+      format: string,
+      height: number,
+      src: string,
+      width: number
+    }) => ({
+      sizes: `${icon.width}x${icon.height}`,
+      src: icon.src,
+      type: `image/${icon.format}`
     })),
     launch_handler: {
       client_mode: 'focus-existing'
